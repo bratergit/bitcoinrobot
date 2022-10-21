f14c56c2726756be88343e9494670cffc7d467a5
adalbertobrant <adalbertobrant@gmail.com>
2022-10-21 06:40:38 -0300

    add index.js market price

--- index.js
+++ index.js
@@ -7,7 +7,34 @@ const tick = async() => {
   const market = `${asset}/${base}`;
 
   const orders = await binanceClient.fetchOpenOrders(market);
+
+  orders.forEach(async order => {
+    await binanceClient.cancelOrders(order.id);
+  });
+
+  const results = await Promise.all([
+    axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'),
+    axios.get('https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=usd')
+  ]);
+  const marketPrice = results[0].data.bitcoin.usd / results[1].data.tether.usd;
   
+
+  const sellPrice = marketPrice * (1 + spread);
+  const buyPrice = marketPrice * (1 - spread);
+  const balance = await binanceClient.fetchBalance();
+  const assetBalance = balances.free[asset];
+  const baseBalance = balances.free[base];
+  const sellVolume = assetBalance * allocation;
+  const buyVolume = (baseBalance * allocation) / marketPrice;
+
+  await binanceClient.createLimitSellOrder(market, sellVolume, sellPrice);
+  await binanceClient.createLimitBuyOrder(market, buyVolume, buyPrice);
+
+  console.log(`
+    New tick for ${market}...
+    Created limit sell order for ${sellVolume}@${sellPrice}
+    Create limit buy order for ${buyVolume}@${buyPrice}
+  `);
 
   }
 
@@ -20,7 +47,7 @@ const run = () => {
    tickInterval: 2000
   };
  const binanceClient = new ccxt.binance({
-  apikey: process.env.API_KEY, 
+  apikey: process.env.API_ENV, 
   secret: process.env.API_SECRET
  });
  
